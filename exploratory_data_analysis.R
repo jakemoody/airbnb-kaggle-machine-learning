a123780cfb25ac873e7d56101cfd6bb95860e29d
Jake Moody <jake@datacamp.com>
2016-09-09 09:59:10 -0400

    update scripts with decision tree

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -14,6 +14,7 @@ library(rpart)
 library(randomForest)
 library(lubridate)
 library(stringr)
+library(plotly)
 
 # IMPORT DATA
 train <- read.csv("/Users/JakeMoody/Dropbox/GitHub Projects/data/airbnb/train_users_2.csv")
@@ -46,10 +47,14 @@ train$date_account_created <- as.Date(train$date_account_created, format="%Y-%m-
 train$timestamp_first_active <- as.Date(as.character(train$timestamp_first_active),"%Y%m%d%H%M%S") # transform to date object
 
 # Clean customer characteristics
-train$gender <- as.factor(gsub("-unknown-", "NA", train$gender))
+#train$gender <- as.factor(gsub("-unknown-", "NA", train$gender))
 
 # Clean browser data
-train$first_browser <- as.factor(gsub("-unknown-", "NA", train$first_browser))
+#train$first_browser <- as.factor(gsub("-unknown-", "NA", train$first_browser))
+#table(train$gender)
+
+# Fix age values
+train <- train[train$age <= 100,] # only ages less than 100
 
 ###### CREATE DUMMY VARIABLES ###### 
 # Time between events
@@ -57,22 +62,63 @@ train$time_to_booking <- as.numeric(train$date_first_booking - train$date_accoun
 train$time_to_account <- as.numeric(train$date_account_created - train$timestamp_first_active) # time between first visit to creating an account
 train$total_time_to_booking <- as.numeric(train$date_first_booking - train$timestamp_first_active) # total time difference
 
+# PREDICT NA age VALUES
+predicted_age <- rpart(age ~ signup_app + first_device_type + first_browser,  data = train[!is.na(train$age),], method = "anova")
+train$age[is.na(train$age)] <- predict(predicted_age, train[is.na(train$age),])
 
-# 
+hist(train$age)
+boxplot(train$age)
+
+# millenial bracket 
 train$millenial <- NA
 train$millenial[train$age < 35] <- 1
 train$millenial[train$age >= 35] <- 0
-unique(train$affiliate_channel)
-prop.table(table(train$millenial, train$country_destination), 1)
-
-unique(train$first_browser)
-# PREDICT NA VALUES
-predicted_age <- rpart(age ~ language + gender + signup_app + first_device_type + first_browser , data = train[!is.na(train$age),], method = "class")
-all_data$Age[is.na(all_data$Age)] <- predict(predicted_age, all_data[is.na(all_data$Age),])
-
 
 # Decision Trees
+my_tree <- rpart(country_destination ~ language + gender + age, data = train, method = "class")
 
+# Clean dates
+test$date_first_booking <- as.Date(test$date_first_booking, format="%Y-%m-%d") # transform to date object
+test$date_account_created <- as.Date(test$date_account_created, format="%Y-%m-%d") # transform to date object
+test$timestamp_first_active <- as.Date(as.character(test$timestamp_first_active),"%Y%m%d%H%M%S") # transform to date object
+
+# Clean customer characteristics
+#train$gender <- as.factor(gsub("-unknown-", "NA", train$gender))
+
+# Clean browser data
+#train$first_browser <- as.factor(gsub("-unknown-", "NA", train$first_browser))
+#table(train$gender)
+
+# Fix age values
+test <- test[test$age <= 100,] # only ages less than 100
+
+###### CREATE DUMMY VARIABLES ###### 
+# Time between events
+test$time_to_booking <- as.numeric(test$date_first_booking - test$date_account_created) # time it took between creating an account and 1st booking
+test$time_to_account <- as.numeric(test$date_account_created - test$timestamp_first_active) # time between first visit to creating an account
+test$total_time_to_booking <- as.numeric(test$date_first_booking - test$timestamp_first_active) # total time difference
+
+# PREDICT NA age VALUES
+predicted_age <- rpart(age ~ signup_app + first_device_type + first_browser,  data = test[!is.na(test$age),], method = "anova")
+test$age[is.na(test$age)] <- predict(predicted_age, test[is.na(test$age),])
+
+
+
+
+## APPLY PREDICTION DECISION TREE
+
+my_prediction <- predict(my_tree, test, type = "class")
+my_solution <- data.frame(id = test$id, country = my_prediction)
+
+
+
+write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
+nrow(my_solution)
+
+
+class(train$language)
+class(train$gender)
+class(train$country_destination)
 # RANDOM FOREST 
 my_forest <- randomForest(country_destination ~ age + language, data = train, importance = TRUE, ntree = 1000)
 
